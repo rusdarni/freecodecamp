7e708925e7a6c8f2e965bf0590d82d08d83db327
dasha <rus.dar.ni@gmail.com>
2024-07-18 14:25:27 +0300

    comment-your-javascript-code.js

---
+++ javascript/comment-your-javascript-code.js
@@ -0,0 +1,13 @@
+// DESCRIPTION:
+// Try creating one of each type of comment.
+
+//SOLUTION:
+// Hey, I'm a comment!
+
+/*
+      Hey, I'm a paragraph comment.
+      This allows for programmers to
+      write tons and tons of random
+      words, without the fear of me
+      being compiled!
+    */
