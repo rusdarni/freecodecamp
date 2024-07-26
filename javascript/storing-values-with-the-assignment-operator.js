12a7dd82e7d764f003383185306086721de1fc83
dasha <rus.dar.ni@gmail.com>
2024-07-26 14:56:19 +0300

    declare-javascript-variables.js

---
+++ javascript/storing-values-with-the-assignment-operator.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// Assign the value 7 to variable a.
+
+//SOLUTION:
+
+var a;
+a = 5; // The variable 'a' is equal to 5
+
+var abc;
+abc = 123; // The variable 'abc' is equal to 123
