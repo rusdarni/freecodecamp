b74d79afb9aaed411b041590604386103f779432
dasha <rus.dar.ni@gmail.com>
2024-07-26 14:58:43 +0300

    declare-javascript-variables.js

---
+++ javascript/assigning-the-value-of-one-variable-to-another.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// Assign the contents of a to variable b.
+
+//SOLUTION:
+
+var a;
+a = 5; // The variable 'a' is equal to 5
+
+var abc;
+abc = a; // The variable 'abc' is equal to 5
