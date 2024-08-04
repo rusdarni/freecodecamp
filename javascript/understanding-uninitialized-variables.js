3bdb5ede37fc9692b9f85bc3c2dadcb8be931e20
dasha <rus.dar.ni@gmail.com>
2024-08-04 20:24:33 +0300

    understanding-uninitialized-variables.js

---
+++ javascript/understanding-uninitialized-variables.js
@@ -0,0 +1,14 @@
+// DESCRIPTION:
+// Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively so that they will not be undefined.
+
+//SOLUTION:
+
+
+var a = 5;
+var b = 10;
+var c = 'I am a';
+
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
