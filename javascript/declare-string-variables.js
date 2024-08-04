0f6ad8f96cff8a0d78242db669acf0423ccee9b8
dasha <rus.dar.ni@gmail.com>
2024-08-04 20:18:53 +0300

    declare-string-variables.js

---
+++ javascript/declare-string-variables.js
@@ -0,0 +1,9 @@
+// DESCRIPTION:
+// Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
+
+//SOLUTION:
+
+var a;
+a = "Hello Camper!";
+
+var myName = "your name";
