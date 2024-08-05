38553e2d796bc956c1c8f8dadbe8f4e373abcacf
dasha <rus.dar.ni@gmail.com>
2024-08-05 19:47:38 +0300

    increment-a-number-with-javascript.js

---
+++ javascript/increment-a-number-with-javascript.js
@@ -0,0 +1,13 @@
+// DESCRIPTION:
+// Change the code to use the ++ operator on myVar.
+//let myVar = 87;
+
+// Only change code below this line
+//myVar = myVar + 1;
+
+// SOLUTION:
+
+let myVar = 87;
+
+// Only change code below this line
+myVar++;
