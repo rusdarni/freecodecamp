bda11fbb48aa08b0506abc7f41716e3bb3f49a8d
dasha <rus.dar.ni@gmail.com>
2024-08-05 19:11:01 +0300

    declare-a-read-only-variable-with-the-const-keyword.js

---
+++ javascript/declare-a-read-only-variable-with-the-const-keyword.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// Change the code so that all variables are declared using let or const. Use let when you want the variable to change, and const when you want the variable to remain constant. Also, rename variables declared with const to conform to common practices. Do not change the strings assigned to the variables.
+
+// SOLUTION:
+
+const FCC = "freeCodeCamp";
+let fact = "is cool!";
+
+fact = "is awesome!";
+console.log(FCC, fact);
