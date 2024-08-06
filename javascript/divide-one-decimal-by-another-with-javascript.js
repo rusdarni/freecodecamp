c151a2a110eb866055c1b13dde60830aba6e8abe
dasha <rus.dar.ni@gmail.com>
2024-08-06 23:20:03 +0300

    divide-one-decimal-by-another-with-javascript.js

---
+++ javascript/divide-one-decimal-by-another-with-javascript.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// Now let's divide one decimal by another.
+//
+// Change the 0.0 so that quotient will equal to 2.2.
+
+// const quotient = 0.0 / 2.0; // Change this line
+//
+// SOLUTION:
+
+var quotient = 4.4 / 2.0; // Fix this line
