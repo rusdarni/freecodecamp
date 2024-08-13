6bce6a91f17aef70e30713bdece59b91f14e2a30
dasha <rus.dar.ni@gmail.com>
2024-08-14 00:02:32 +0300

    manipulate-arrays-with-pop-method.js

--- javascript/manipulate-arrays-with-pop-method.js
+++ javascript/manipulate-arrays-with-pop-method.js
@@ -2,7 +2,8 @@
 // Another way to change the data in an array is with the .pop() function.
 //
 // .pop() is used to pop a value off of the end of an array.
-// We can store this popped off value by assigning it to a variable. In other words, .pop() removes the last element from an array and returns that element.
+// We can store this popped off value by assigning it to a variable.
+// In other words, .pop() removes the last element from an array and returns that element.
 //
 // Any type of entry can be popped off of an array - numbers, strings, even nested arrays.
 //
