1555cf73c97b9f378b24add54d7bc46eaaaab39c
dasha <rus.dar.ni@gmail.com>
2024-08-22 01:00:43 +0300

    using-objects-for-lookups.js

--- javascript/using-objects-for-lookups.js
+++ javascript/using-objects-for-lookups.js
@@ -26,7 +26,7 @@
 //
 // Convert the switch statement into an object called lookup.
 // Use it to look up val and assign the associated string to the result variable.
-Setup
+// Setup
 // function phoneticLookup(val) {
 //     let result = "";
 //
